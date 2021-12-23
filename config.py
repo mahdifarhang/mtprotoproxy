0f370284480e32fe831799b494965ac9e1213ade
Mahdi <mafarhang76@gmail.com>
2021-12-23 15:06:28 +0330

    users single DICT !!

--- config.py
+++ config.py
@@ -8,19 +8,17 @@ PORT = 443
 # GENERATE RANDOM SECRET TOKEN:
 # hexdump -n 40 -e '4/4 "%08X" 1 "\n"' /dev/urandom
 
-PAYINGUsers = {
+USERS = {
+	# PAYINGUsers
 
-}
 
-VIPUsers = {
+	# VIPUsers
 	"mahdi": "8c30229c8a693d2c242e74f960d244a3",
 	"maha": "c7fd27b44680bdea99b29add6a509b8a",
 	"maman": "682363a07be5c604a72249dcc7d0771a",
 	"baba": "c4c997d116bdbe47ae2593d3e2b71f74",
 	"morteza": "e7e0335bcf5879bea79312f1dd9923a2",
 }
-
-USERS = {**PAYINGUsers, **VIPUsers}
 
 MODES = {
 	# Classic mode, easy to detect
